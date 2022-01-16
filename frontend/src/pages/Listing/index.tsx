68bbcec788c45a17e950861ffbf6dab14b419510
Marcelo Sena <marcelomarcos2@gmail.com>
2022-01-16 20:24:53 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,32 @@
+import MovieCard from "Components/MovieCard";
 import Pagination from "Components/Pagination";
 
-function Listing(){
-    return(
+function Listing() {
+  return (
+    <>
+      <Pagination />
 
-       <Pagination/>
-
-    );
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
